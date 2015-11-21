a9c738a540cabdef0acb42357c930c316cabdd4c
fanaimi <emiliano.fantasia@gmail.com>
2015-11-21 17:51:45 +0000

    sine wave and buttons

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # canvas_trigo_functions
 Fun with canvas and trigonometry
+Not ignoring dev folder to show non minified files and comments.
 
-
-1, Drawing and animating Sine wave
+## 1, Drawing and animating Sine wave
--- dev/js/main.js
+++ dev/js/main.js
@@ -2,7 +2,6 @@
  * @author      fanaimi, emiliano fantasia
  * @file        main => output
  * @desc        playing with canvas and trigonometry
- * @inspiration http://krazydad.com/tutorials/circles/
  *      x = cx + cos(θ) R
  *      y = cy + sin(θ) R
  */
@@ -15,12 +14,49 @@ var
     h = c1.height,
     af = null,
     c = 0,
+    paused = false,
+    angleSpeed = 1,
 
     degToRad = function(deg){
         return deg * (Math.PI / 180);
     }, //degToRad
 
     rDot = null,
+    waveDots = [],
+    waveSpeed = 2,
+
+    WaveDot = function(x, y, ctx, waveSpeed, leftEdge){
+        this.x = x;
+        this.y = y;
+        this.r = 4;
+        this.ctx = ctx;
+        this.waveSpeed = waveSpeed;
+
+        this.draw = function(){
+            this.ctx.beginPath();
+            this.ctx.arc(this.x, this.y, this.r, 0, 2* Math.PI, true);
+            this.ctx.fillStyle = "red";
+            this.ctx.fill();
+            this.ctx.closePath();
+        }; // draw
+
+
+        this.move = function(){
+            if(this.x > leftEdge - this.r){
+                this.x -= this.waveSpeed;
+            } else {
+                for (var l in waveDots) {
+                    if (waveDots[l] == this) {
+                        waveDots.splice(l, 1)
+                    }
+                }
+            }// if
+
+        };//move
+
+
+
+    },
 
     RedDot = function(x, y, r, ctx, cx, cy, cRad, speedAngle){
         this.x = x;
@@ -28,6 +64,7 @@ var
         this.r = r;
         this.angle = 90;
         this.ctx = ctx;
+        this.speedAngle = speedAngle;
 
 
         this.draw = function(){
@@ -42,7 +79,7 @@ var
         }; // draw
 
         this.move = function(){
-            this.angle += speedAngle;
+            this.angle += this.speedAngle;
 
             this.x = cx + Math.cos( degToRad( this.angle ) ) * cRad;
             this.y = cy + Math.sin( degToRad( this.angle ) ) * cRad;
@@ -57,7 +94,7 @@ var
     drawC1 = function(){
         ctx1.beginPath();
         ctx1.arc(200, 200, 150, 0, 2* Math.PI, true);
-        ctx1.strokeStyle = "#000";
+        ctx1.strokeStyle = "green";
         ctx1.lineWidth = 2;
         ctx1.stroke();
         ctx1.closePath();
@@ -67,14 +104,14 @@ var
     drawCentre = function(){
         ctx1.beginPath();
         ctx1.arc(200, 200, 3, 0, 2* Math.PI, true);
-        ctx1.fillStyle = "#000";
+        ctx1.fillStyle = "red";
         ctx1.fill();
         ctx1.closePath();
     }, //drawCentre
 
 
     addRedDot = function(){
-        rDot = new RedDot(250, 50, 5, ctx1, 200, 200, 150, 2);
+        rDot = new RedDot(250, 50, 5, ctx1, 200, 200, 150,angleSpeed);
         rDot.draw();
     }
 
@@ -92,7 +129,35 @@ var
         ctx1.stroke();
         ctx1.beginPath()
 
-    }
+    },
+
+    drawRadius = function(){
+        ctx1.beginPath()
+        ctx1.moveTo(200, 200);
+        ctx1.lineTo(rDot.x, rDot.y);
+        ctx1.strokeStyle = "blue";
+        ctx1.lineWidth = 1;
+        ctx1.stroke();
+        ctx1.beginPath()
+    }, //drawRadius
+
+
+    addNewWaveDot = function () {
+        var waveDot = new WaveDot(w, rDot.y, ctx1, waveSpeed, 400);
+        waveDots.push(waveDot);
+        waveDot.draw();
+    }, //addNewWaveDot
+
+    moveWaveDots = function () {
+       /* for(let i of waveDots){
+            i.move();
+            i.draw();
+        }*/
+        for(var i in waveDots){
+            waveDots[i].move();
+            waveDots[i].draw();
+        }
+    }, // moveWaveDots
 
     playLoop = function () {
 
@@ -100,19 +165,87 @@ var
         drawC1();
         drawCentre();
         moveRedDot();
+        drawRadius();
         drawGuide();
-        //c+=1;
-        //console.log(c);
+
+        addNewWaveDot();
+        moveWaveDots();
+
         af = window.requestAnimationFrame(playLoop)
     }, //playLoop
+
+    pauseLoop = function(){
+        console.log("paused");
+        window.cancelAnimationFrame(af);
+    }, // pauseLoop
+
+    bindBtns = function () {
+        document.getElementById("pauseBtn")
+            .addEventListener("click", function(){
+                pauseLoop();
+                paused = true;
+            });
+
+
+        document.getElementById("restoreBtn")
+            .addEventListener("click", function(){
+                if(paused){ playLoop(); paused = false}
+            });
+
+        //==================
+        document.getElementById("slowAngleBtn")
+            .addEventListener("click", function(){
+                rDot.speedAngle --;
+            });
+
+
+        document.getElementById("pauseAngleBtn")
+            .addEventListener("click", function(){
+                rDot.speedAngle = 0;
+            });
+
+        document.getElementById("fastAngleBtn")
+            .addEventListener("click", function(){
+                rDot.speedAngle ++;
+            });
+        //==================
+
+        document.getElementById("slowGridBtn")
+            .addEventListener("click", function(){
+                for(var i in waveDots){
+                    waveDots[i].waveSpeed --;
+                }
+            });
+
+        document.getElementById("pauseGridBtn")
+            .addEventListener("click", function(){
+                for(var i in waveDots){
+                    waveDots[i].waveSpeed =0;
+                }
+            });
+
+        document.getElementById("fastGridBtn")
+            .addEventListener("click", function(){
+                for(var i in waveDots){
+                    waveDots[i].waveSpeed ++;
+                }
+            });
+
+
+
+
+    }, //bindBtns
+
 
 
     init = function(){
 
         addRedDot();
         playLoop();
+        bindBtns();
     } // init
 ;
 
+    window.addEventListener("blur", pauseLoop);
 window.addEventListener("load", init);
 })();
